216ac0da237f660c1d380bb4dd7bbc3eaf603528
arr213 <arr213@gmail.com>
2017-02-08 18:46:55 -0500

    port update

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 // request any page and receive index.html
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-app.listen(3000, function () {
-  console.log('Server listening on port', 3000);
+var PORT = process.env.PORT || 1337
+app.listen(PORT, function () {
+  console.log('Server listening on port', PORT);
 });
 
